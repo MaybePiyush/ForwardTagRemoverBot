8011238a26893341694adaff168dd2658eb06f89
ᴍᴀʏʙᴇ ᴘɪʏᴜꜱʜ <124165177+MaybePiyush@users.noreply.github.com>
2023-02-24 22:10:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
+    SOURCE = "https://github.com/MaybePiyush/ForwardTagRemoverBot"
+    START_TEXT = """
+Hi [{}](tg://user?id={}) 
+I am A Forward Tag remover Bot.
+Send /help to know more ©[Abdul Razaq](https://github.com/MaybePiyush)"""
+    HELP_TEXT = "Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
